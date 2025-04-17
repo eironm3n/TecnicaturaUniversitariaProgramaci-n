b3a2ff065c543d3781386653763b33ddf07d1235
Arón Rojas <c1.aronrojas@gmail.com>
2025-04-17 20:19:28 -0300

    Se agrega Clase Computadora y Orden

---
+++ _3erSemestre/3. Java_3/Clase1/MundoPc/src/ar/com/system2025/mundopc/Orden.java
@@ -0,0 +1,16 @@
+/*
+    Clase Orden
+ */
+package ar.com.system2025.mundopc;
+
+
+public class Orden {
+    private final int idOrden;
+    private Computadora computadora[];  //Arreglo de objetos
+    private static int contadorOrdenes;
+    private static final int MAX_COMPUTADORAS = 10;
+    private int contadorComputadoras;
+    
+    
+    
+}
