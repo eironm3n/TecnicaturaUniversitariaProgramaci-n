f684960a586be9619d4a1cf61e7f0dc583bc0981
Arón Rojas <c1.aronrojas@gmail.com>
2025-04-17 20:45:29 -0300

    Ya casi finalizamos

---
+++ _3erSemestre/3. Java_3/Clase1/MundoPc/src/mundopc/mundoPC.java
@@ -0,0 +1,29 @@
+/*
+    Se agrega paquete mundopc, y la clase mundoPC.java
+ */
+
+package mundopc;
+
+import ar.com.system2025.mundopc.*;     //al agregar '.*' se importan todas las clases desde el otro paquete.
+
+
+public class mundoPC {
+    public static void main(String[] args) {
+        Monitor monitorHP = new Monitor("HP", 13);  //Importar la clase 
+        Teclado tecladoHP = new Teclado("Bluetooth", "HP");
+        Raton ratonHP = new Raton("Bluetooth","HP");
+        Computadora computadoraHP = new Computadora("Computadora HP", monitorHP, tecladoHP, ratonHP);
+        
+        //Creamos otros objetos de diferente marca
+        Monitor monitorGamer = new Monitor("Gamer", 32); 
+        Teclado tecladoGamer= new Teclado("Bluetooth", "Gamer");
+        Raton ratonGamer = new Raton("Bluetooth","Gamer");
+        Computadora computadoraGamer = new Computadora("Computadora Gamer", monitorGamer, tecladoGamer, ratonGamer);
+        Orden orden1 = new Orden(); //Inicializamos el arreglo vacio
+        orden1.agregarComputadora(computadoraHP);
+        orden1.agregarComputadora(computadoraGamer);
+        orden1.mostrarOrden();
+    }
+    
+    
+}
