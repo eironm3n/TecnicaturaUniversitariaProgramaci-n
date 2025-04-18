d3653f258c3a9d48fabce88686c95e2e5f9425fd
Arón Rojas <c1.aronrojas@gmail.com>
2025-04-18 13:37:45 -0300

    Finalizada Clase 2

--- _3erSemestre/3. Java_3/Clase2/BloquesInicializacion/src/domain/Persona.java
+++ _3erSemestre/3. Java_3/Clase2/BloquesInicializacion/src/domain/Persona.java
@@ -30,6 +30,11 @@ public class Persona {
     public int getIdPersona() {
         return this.idPersona;
     }
+
+    @Override
+    public String toString() {
+        return "Persona{" + "idPersona=" + idPersona + '}';
+    }
     
     
     
---
+++ _3erSemestre/3. Java_3/Clase2/BloquesInicializacion/src/test/TestBloqueInicialización.java
@@ -0,0 +1,16 @@
+/*
+Se crea la clase Test y se testea la clase Domain.java
+ */
+package test;
+
+import domain.Persona;
+
+
+public class TestBloqueInicialización {
+    public static void main(String[] args) {
+        Persona persona1 = new Persona();
+        System.out.println("persona1 = " + persona1);   //esto nos muestra la referencia del objeto}
+        Persona persona2 = new Persona();
+        System.out.println("persona2 = " + persona2);
+    }
+}
