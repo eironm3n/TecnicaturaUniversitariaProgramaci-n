56377f4675588d085786d22d39ad7c14e70ca5bc
Arón Rojas <c1.aronrojas@gmail.com>
2025-04-18 13:15:25 -0300

    bloque estatico en Java

---
+++ _3erSemestre/3. Java_3/Clase2/BloquesInicializacion/src/domain/Persona.java
@@ -0,0 +1,21 @@
+/*
+Continuamos con Bloques de Inicialización
+Tenemos bloques de Inicialización estáticos y NoEstaticos.
+Ambos se van a ejecutar antes del constructor, de lo que es nuestra clase
+ */
+package domain;
+
+
+public class Persona {
+    private final int idPersona;
+    private static int contadorPersonas;
+    
+    //creamos bloque de inicialización estatico
+    static{
+        System.out.println("Ejecución del bloque estático");
+        ++Persona.contadorPersonas;
+        //idPersona = 10; --> Esto genera un error, ya que no es un atributo estatico
+    }
+    
+    
+}
