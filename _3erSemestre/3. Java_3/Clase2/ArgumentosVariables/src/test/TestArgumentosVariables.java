0f51bd172ddf1527cc68de9c5c0da96668967d70
Arón Rojas <c1.aronrojas@gmail.com>
2025-04-18 11:53:04 -0300

    Update

--- _3erSemestre/3. Java_3/Clase2/ArgumentosVariables/src/test/TestArgumentosVariables.java
+++ _3erSemestre/3. Java_3/Clase2/ArgumentosVariables/src/test/TestArgumentosVariables.java
@@ -1,14 +1,20 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+    Clase 2 de Java
+
  */
 package test;
 
-/**
- *
- * @author Arón
- */
 public class TestArgumentosVariables {
-    
+    public static void main(String[] args) {
+        
+    }
+    // Metodo Imprimir Numeros
+    public static void imprimirNumeros(int ...numeros) {
+        //los ... pertenecen al argumento variable
+        //int ...numeros esto es un arreglo que indica que se agregara una cantidad indefinida de argumentos
+        
+        for (int i = 0; i < numeros.length; i++){
+            System.out.println("Elementos: "+numeros[i]);
+        }
+    }
 }
