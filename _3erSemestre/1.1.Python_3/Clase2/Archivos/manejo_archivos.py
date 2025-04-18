082e092b780bcad1856349944826c992681f6409
Arón Rojas <c1.aronrojas@gmail.com>
2025-04-18 17:25:03 -0300

    1. Introducción a lo que es el manejo de archivos

--- _3erSemestre/1.1.Python_3/Clase2/Archivos/manejo_archivos.py
+++ _3erSemestre/1.1.Python_3/Clase2/Archivos/manejo_archivos.py
@@ -1,10 +1,18 @@
-# Declaramos una variable
+"""
+Con 'open' verificamos que si el archivo prueba.txt no existe, que lo genere e ingrese.
+Luego con 'w' hacemos que "escriba" en él
+De momento, este archivo prueba.txt, en caso de no encontrarlo dentro de la Raiz de esta carpeta, lo generará
+Caso contrario, abrirá este mismo.
+"""
+
 try:
-    archivo = open('prueba.txt', 'w')  # la W es por write
-    #este metodo nos ayuda para incluir información hacia un archivo o abrir otro
+    archivo = open('prueba.txt', 'w')  # La W es por write
+    # Este metodo nos ayuda para incluir información hacia un archivo o abrir otro
+    archivo.write('Programamos con diferentes tipos de archivos, ahora en txt.\n')
+    archivo.write('Con esto terminamos.')
+
 except Exception as e:
     print(e)
-finally:    #siempre se ejecuta
-    archivo.close() #Con esto se debe cerrar el archivo
+finally:    # Siempre se ejecuta
+    archivo.close() # Con esto se debe cerrar el archivo
 
-    
