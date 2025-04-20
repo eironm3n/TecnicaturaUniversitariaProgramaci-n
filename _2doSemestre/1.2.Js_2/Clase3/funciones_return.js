f2618596d661ef4d94b20341372273a1469bb04b
Arón Rojas <c1.aronrojas@gmail.com>
2025-04-20 13:12:28 -0300

    Update funciones_return.js

---
+++ _2doSemestre/1.2.Js_2/Clase3/funciones_return.js
@@ -0,0 +1,13 @@
+/* return
+Esta instrucción 'return' en JavaScript se utiliza para finalizar la 
+ejecución de una función y devolver un valor al origen de la llamada.
+Permite que la función proporcione resultados útiles, como números, 
+cadenas u objetos, que pueden usarse en otras partes del código.
+*/
+
+function miFuncion(a,b){
+    return a + b;
+}
+
+let resultado = miFuncion(6,7);
+console.log
