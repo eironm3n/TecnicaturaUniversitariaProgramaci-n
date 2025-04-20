fadddbeba66e3110309ce6e2a86c268d3baed890
Arón Rojas <c1.aronrojas@gmail.com>
2025-04-20 13:03:01 -0300

    Se comienza con Clase3, intro a Funciones

---
+++ _2doSemestre/1.2.Js_2/Clase3/funciones.js
@@ -0,0 +1,13 @@
+/*
+Introducción a Funciones
+Para las funciones se utiliza CamelCase (averiguar si se puede otro tipo de escritura)
+
+*/ 
+
+function miFuncion(a,b){
+    console.log("Sumamos: "+ (a+b));
+}
+
+// Llamamos a la función
+miFuncion(5,4);
+
