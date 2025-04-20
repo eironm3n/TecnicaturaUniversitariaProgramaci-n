d0170a737692b6588338de14c106bcdd6bd6b51b
Arón Rojas <c1.aronrojas@gmail.com>
2025-04-20 13:05:36 -0300

    update

--- _2doSemestre/1.2.Js_2/Clase3/funciones_intro.js
+++ _2doSemestre/1.2.Js_2/Clase3/funciones_intro.js
@@ -4,6 +4,8 @@ Para las funciones se utiliza CamelCase (averiguar si se puede otro tipo de escr
 
 */ 
 
+miFuncion(8,2)      // a esto se lo conoce como hoisting, se puede hacer el llamado a la funcion incluso antes de definirla
+
 function miFuncion(a,b){
     console.log("Sumamos: "+ (a+b));
 }
