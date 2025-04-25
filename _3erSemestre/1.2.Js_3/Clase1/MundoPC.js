9ca968dca8f00b0681ade52c72eb43f24065f4f4
Arón Rojas <c1.aronrojas@gmail.com>
2025-04-25 20:02:38 -0300

    Update mundopc.js pero se debe revisar y concluir de forma exitosa.

--- _3erSemestre/1.2.Js_3/Clase1/MundoPC.js
+++ _3erSemestre/1.2.Js_3/Clase1/MundoPC.js
@@ -69,3 +69,82 @@ let teclado1 = new Teclado('USB', 'HP');
 console.log(teclado1.toString());
 let teclado2 = new Teclado('Bluetooth','MSI');
 console.log(teclado2.toString());
+
+class Monitor{
+    static contadorMonitores = 0;
+
+    constructor(marca, tamanio){
+        this._idMonitor = ++Monitor.contadorMonitores;
+        this._marca = marca;
+        this._tamanio = tamanio;
+    }
+
+    get idMonitor(){
+        return this._idMonitor;
+    }
+
+    toString(){
+        return `Monitor: [idMonitor: ${this._idMonitor}, marca: ${this._marca}, tamaño: ${this._tamanio}]`;
+    }
+}
+
+let monitor1 = new Monitor('HP', 15);
+let monitor2 = new Monitor('Dell', 27);
+console.log(monitor1.toString());
+console.log(monitor2.toString());
+
+class Computadora {
+    static contadorComputadoras = 0;
+
+    constructor(nombre, Monitor, Raton, Teclado){
+        this._idComputadora = ++Computadora.contadorComputadoras;
+        this._nombre = nombre;
+        this._monitor = monitor;
+        this._raton = raton;
+        this._teclado = teclado;
+    }
+    toString(){
+        return `Computadora ${this._idComputadora}: ${this._nombre}\n ${this._monitor}\n ${this._raton}\n ${this._teclado}`;
+    } 
+}
+
+let computadora1 = new Computadora('HP', monitor1, raton1, teclado1);
+console.log(computadora1.toString());
+console.log(`${Computadora1}`)  //llama automaticamente al toString
+let computadora2 = new Computadora('Aser', monitor2, raton2, teclado2);
+console.log(computadora2.toString());
+console.log(`${Computadora2}`)
+
+class Orden {
+    static contadorOrdenes = 0;
+
+    constructor(){
+        this._idOrden = ++Orden.contadorOrdenes;
+        this._computadoras = [];
+    }
+
+    agregarComputadora(Computadora){
+        this._computadoras.push(Computadora);   //Agregamos los objetos
+    }
+
+    mostrarOrden(){
+        let computadorasOrden = '';
+        for(let Computadora of this._computadoras){
+            computadorasOrden += '\n${computadora}';
+        }
+        console.log(`Orden: ${this._idOrden}, Computadoras: ${computadorasOrden}`);
+    }
+}
+
+let orden1 = new Orden();
+orden1.agregarComputadora(computadora1);
+orden1.agregarComputadora(computadora2);
+orden1.agregarComputadora(computadora2);    //se puede repetir una orden
+orden1.mostrarOrden();
+
+let orden2 = new Orden();
+orden2.agregarComputadora(computadora2);
+orden2.agregarComputadora(computadora1);
+orden2.mostrarOrden();
+
+//TERMINAR, TIENE ERRORES
