8f21429465a24059d5a2f485c7c7a56ded713c20
Arón Rojas <c1.aronrojas@gmail.com>
2025-04-24 21:17:35 -0300

    update y practica

---
+++ _3erSemestre/1.2.Js_3/Clase1/MundoPC.js
@@ -0,0 +1,71 @@
+
+class DispositivoEntrada{
+    constructor(tipoEntrada, marca){
+        this._tipoEntrada = tipoEntrada;
+        this._marca = marca;
+    }
+    
+    get tipoEntrada(){
+        return this._tipoEntrada;
+    }
+
+    set tipoEntrada(tipoEntrada){
+        this._tipoEntrada = tipoEntrada;
+    }
+
+    get marca(){
+        return this._marca;
+    }
+
+    set marca(marca){
+        this._marca = marca;
+    }
+
+}
+
+
+class Raton extends DispositivoEntrada{
+    static contadorRatones = 0;
+
+    constructor(tipoEntrada, marca){
+        super(tipoEntrada, marca);
+        this._idRaton = ++Raton.contadorRatones;
+    }
+
+    get idRaton(){
+        return this._idRaton;
+    }
+
+    toString(){
+        return `Raton: [idRaton: ${this._idRaton}, tipoEntrada: ${this._tipoEntrada}, marca: ${this._marca}]`;
+    }
+
+}
+
+let raton1 = new Raton('USB','HP');
+console.log(raton1.toString());
+let raton2 = new Raton('Bluetooth','Dell');
+console.log(raton2.toString());
+
+class Teclado extends DispositivoEntrada{
+    static contadorTeclado = 0;
+
+    constructor(tipoEntrada, marca){
+        super(tipoEntrada,marca);
+        this._idTeclado = ++Teclado.contadorTeclado;
+    }
+
+    get _idTeclado(){
+        return this._idTeclado;
+    }
+
+    toString(){
+        return `Teclado: [idTeclado: ${this._idTeclado}, tipoEntrada: ${this._tipoEntrada}, marca: ${this._marca}`;
+    }
+
+}
+
+let teclado1 = new Teclado('USB', 'HP');
+console.log(teclado1.toString());
+let teclado2 = new Teclado('Bluetooth','MSI');
+console.log(teclado2.toString());
