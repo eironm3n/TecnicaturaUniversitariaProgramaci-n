8bbdf72d869f24fe75da050566b42c695b82fd20
Arón Rojas <c1.aronrojas@gmail.com>
2025-04-26 22:58:49 -0300

    Comenzamos con las pruebas ingresando las otras clases, parte 1

--- _3erSemestre/1.1.Python_3/Clase3/catalogo_peliculas/test_catalogo_peliculas.py
+++ _3erSemestre/1.1.Python_3/Clase3/catalogo_peliculas/test_catalogo_peliculas.py
@@ -1,3 +1,7 @@
+from dominio.pelicula import pelicula
+from servicio.catalogo_peliculas import CatalogoPeliculas as cp
+# cuando utilizamos 'as' es para renombrar el nombre completo mencionado en la importacion
+
 opcion = None
 
 while opcion != 4:
@@ -8,6 +12,14 @@ while opcion != 4:
         print('3. Eliminar catalogo de peliculas')
         print('4. Salir')
         opcion = int(input('Digite una opcion de menu (1-4): '))
+        if opcion == 1:
+            nombre_pelicula = input('Digite el nombre de la pelicula: ')
+            pelicula = pelicula(nombre_pelicula)
+            cp.agregar_peliculas(pelicula)
+        elif opcion == 2:
+            cp.listar_peliculas(pelicula)
+        elif opcion == 3:
+            cp.eliminar_peliculas(pelicula)
     except Exception as e:
         print(f'Ocurrió un error de tipo: {e}')
         opcion = None
