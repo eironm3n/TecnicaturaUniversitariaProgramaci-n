f002c4423de9e28a75c7f0d57d51b41782728a9c
Arón Rojas <83792412+eironm3n@users.noreply.github.com>
2025-05-06 20:06:22 -0300

    Update prueba_bd.py

--- _3erSemestre/1.1.Python_3/Clase4/BDD/prueba_bd.py
+++ _3erSemestre/1.1.Python_3/Clase4/BDD/prueba_bd.py
@@ -2,7 +2,7 @@
 utilización de librerias psycopg2, perteneciente a Postgresql 
 Instalar en venv = 
 pip install psycopg2
-pip install mysql-connector
+
 """
 
 import psycopg2
@@ -11,15 +11,15 @@ import psycopg2
 #conexion = psycopg2.connect()
 conexion = psycopg2.connect(
     user='postgres',
-    password= 'admin',
+    password='admin',
     host='127.0.0.1',
     port='5432',
     database='test_db'
 )
 # con este metodo podemos conectarnos a la base de datos
 
-print(conexion)
+# print(conexion)
 # esto nos muestra una correcta ejecución del conector a la base de datos ya creada
-"""
-esto es una prueba para github desktop
-"""
+cursor = conexion.cursor()
+sentencia = 'SELECT * FROM persona'
+cursor.execute(sentencia)
