2215134c0d1e471be4565f6dd1b5d684d0dd11b8
Arón Rojas <c1.aronrojas@gmail.com>
2025-05-13 20:37:41 -0300

    Actualizar o modificar un registro

---
+++ _3erSemestre/1.1.Python_3/Clase5/testDB/registros_actualizar.py
@@ -0,0 +1,24 @@
+"""
+actaulizar registros con psycopg2
+"""
+import psycopg2
+conexion = psycopg2.connect(user='postgres',password='admin',host='127.0.0.1',port='5432',database='test_db')
+try:
+    with conexion:
+        with conexion.cursor() as cursor:
+            sentencia = 'UPDATE persona SET nombre=%s, apellido=%s, email=%s WHERE id_persona=%s'
+
+            valores = ('Juan Carlos','Roldan','rcarlos@mail.com',1)
+
+            cursor.execute(sentencia, valores)
+            registros_actualizados = cursor.rowcount
+            print(f'Los registros actualizados son: {registros_actualizados}')
+
+            """
+            Esto deberia arrojar:
+            Los registros actualizados son: 1
+            """
+except Exception as e:
+    print(f'Ocurrio un error: {e}')
+finally:
+    conexion.close()
