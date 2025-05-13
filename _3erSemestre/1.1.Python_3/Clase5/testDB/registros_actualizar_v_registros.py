9e076e8480386805d7346a2e6e6a471769865c6a
Arón Rojas <c1.aronrojas@gmail.com>
2025-05-13 20:49:11 -0300

    Actualizar o modificar varios registros

--- _3erSemestre/1.1.Python_3/Clase5/testDB/registros_actualizar_v_registros.py
+++ _3erSemestre/1.1.Python_3/Clase5/testDB/registros_actualizar_v_registros.py
@@ -1,5 +1,5 @@
 """
-actaulizar varios registros con psycopg2
+actualizar varios registros con psycopg2
 """
 import psycopg2
 conexion = psycopg2.connect(user='postgres',password='admin',host='127.0.0.1',port='5432',database='test_db')
@@ -8,15 +8,19 @@ try:
         with conexion.cursor() as cursor:
             sentencia = 'UPDATE persona SET nombre=%s, apellido=%s, email=%s WHERE id_persona=%s'
 
-            valores = ('Juan Carlos','Roldan','rcarlos@mail.com',1)
+            valores = (
+                ('Moch','castello','mcastello@mail.com', 2),
+                ('Toribia','Tinelli', 'ttinelli@mail.com', 3)
+            )
 
-            cursor.execute(sentencia, valores)
+            cursor.executemany(sentencia, valores)
+            # recordar que si los elementos son varios, se debe agregar many siempre
             registros_actualizados = cursor.rowcount
             print(f'Los registros actualizados son: {registros_actualizados}')
 
             """
             Esto deberia arrojar:
-            Los registros actualizados son: 
+            Los registros actualizados son: 2
             """
 except Exception as e:
     print(f'Ocurrio un error: {e}')
