85bbac8162a43816d2bd76c2accbaa7c3d5b40ef
Arón Rojas <c1.aronrojas@gmail.com>
2025-05-13 20:09:26 -0300

    Insertar un registro con psycopg2

--- _3erSemestre/1.1.Python_3/Clase5/testDB/registros_i_registros.py
+++ _3erSemestre/1.1.Python_3/Clase5/testDB/registros_i_registros.py
@@ -7,15 +7,20 @@ conexion = psycopg2.connect(user='postgres',password='admin',host='127.0.0.1',po
 try:
     with conexion:
         with conexion.cursor() as cursor:
-            sentencia = 'INSERT INTO persona (nombre, apellido, mail) VALUE(%s,%s,%s)'
+            sentencia = 'INSERT INTO persona (nombre, apellido, email) VALUES (%s,%s,%s)'
             valores = ('Carlos','Lara','clara@mail.com')
-            cursor.execute(sentencia,valores)
+            cursor.execute(sentencia, valores)
             
             # conexion.commit()
             # Lo anterior no lo precisamos ya que el uso del with lo realiza
 
             registros_insertados = cursor.rowcount
             print(f'Los registros insertados son: {registros_insertados}')
+
+            """
+            Esto deberia arrojar:
+            Los registros insertados son: 1
+            """
 except Exception as e:
     print(f'Ocurrio un error: {e}')
 finally:
