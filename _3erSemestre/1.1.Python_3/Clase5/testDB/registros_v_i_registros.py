c4d7befc4e0e426899d7a42bf73aaeeba6be4bb0
Arón Rojas <c1.aronrojas@gmail.com>
2025-05-13 20:31:57 -0300

    Update

--- _3erSemestre/1.1.Python_3/Clase5/testDB/registros_v_i_registros.py
+++ _3erSemestre/1.1.Python_3/Clase5/testDB/registros_v_i_registros.py
@@ -1,4 +1,6 @@
 """
+i = insertar
+v = varios
 insertar varios registros con psycopg2
 """
 import psycopg2
@@ -7,14 +9,21 @@ try:
     with conexion:
         with conexion.cursor() as cursor:
             sentencia = 'INSERT INTO persona (nombre, apellido, email) VALUES (%s,%s,%s)'
-            valores = ('Carlos','Lara','clara@mail.com')
-            cursor.execute(sentencia, valores)
+
+            valores = (
+                ('Carlos','Lara','clara@mail.com'),
+                ('Pepe','Mujica','pmujica@mail.com'),
+                ('Michelle','Pfifer','mpfifer@mail.com'),
+            )
+
+            cursor.executemany(sentencia, valores)
+            # similar a la anterior pero ingresa 'many' o varios
             registros_insertados = cursor.rowcount
             print(f'Los registros insertados son: {registros_insertados}')
 
             """
             Esto deberia arrojar:
-            Los registros insertados son: 1
+            Los registros insertados son: 3
             """
 except Exception as e:
     print(f'Ocurrio un error: {e}')
