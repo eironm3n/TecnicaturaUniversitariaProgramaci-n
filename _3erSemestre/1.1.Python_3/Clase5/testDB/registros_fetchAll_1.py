6156d384a4775de762772be179a9316b430ba2e2
Arón Rojas <c1.aronrojas@gmail.com>
2025-05-13 19:48:03 -0300

    Update

--- _3erSemestre/1.1.Python_3/Clase5/testDB/registros_fetchAll_1.py
+++ _3erSemestre/1.1.Python_3/Clase5/testDB/registros_fetchAll_1.py
@@ -4,3 +4,18 @@ uso de fetchAll parte 1
 import psycopg2
 conexion = psycopg2.connect(user='postgres',password='admin',host='127.0.0.1',port='5432',database='test_db')
 
+try:
+    with conexion:
+        with conexion.cursor() as cursor:
+            sentencia = 'SELECT * FROM persona WHERE id_persona IN (1,2)'
+            id_persona = input('Digite un numero para el id_persona: ')
+            cursor.execute(sentencia,(id_persona,))   #  ejecutamos sentencia
+            registros = cursor.fetchall()   # Recuperamos registros
+            for registro in registros:
+                print("Mostrará todas las tuplas 2 veces: ",registros)
+                print("Mostrara cada tupla ordenada: ",registro)
+
+except Exception as e:
+    print(f'Ocurrio un error: {e}')
+finally:
+    conexion.close()
--- _3erSemestre/1.1.Python_3/Clase5/testDB/registros_fetchOne.py
+++ _3erSemestre/1.1.Python_3/Clase5/testDB/registros_fetchOne.py
@@ -9,21 +9,21 @@ try:
         with conexion.cursor() as cursor:
             sentencia = 'SELECT id_persona, nombre FROM persona'
             cursor.execute(sentencia)   #  ejecutamos sentencia
-            registros = cursor.fetchall()   # Recuperamos todos los registros que serán una lista
+            registros = cursor.fetchone()   # Recuperamos todos los registros que serán una lista
             print("Soy un registro: ",registros)
             sentencia2 = 'SELECT * FROM persona WHERE id_persona = 1'
             cursor.execute(sentencia2)   # ejecutamos sentencia
-            registros2 = cursor.fetchall()   # Recuperamos todos los registros que serán una lista
+            registros2 = cursor.fetchone()   # Recuperamos todos los registros que serán una lista
             print("Soy un registro distinto: ",registros2)
             sentencia3 = 'SELECT * FROM persona WHERE id_persona = %s'  #placeholder
             id_persona = 1
             cursor.execute(sentencia3,(id_persona,))   # ejecutamos sentencia y la variable establecida en el placeholder
-            registros3 = cursor.fetchall()   # Recuperamos todos los registros en una lista
+            registros3 = cursor.fetchone()   # Recuperamos todos los registros en una lista
             print("Soy parametro establecido por un placeholder ",registros2)
             sentencia4 = 'SELECT * FROM persona WHERE id_persona = %s'  #placeholder
             id_persona = input('Digite un numero para el id_persona: ')
             cursor.execute(sentencia4,(id_persona,))   # lo mismo pero con input
-            registros4 = cursor.fetchall()   # Recuperamos todos los registros en una lista
+            registros4 = cursor.fetchone()   # Recuperamos todos los registros en una lista
             print("Soy parametro establecido por un placeholder ",registros2)
 except Exception as e:
     print(f'Ocurrio un error: {e}')
