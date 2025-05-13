290d12a4635cd9e3649e9a55c60fed2e1bae05a8
Arón Rojas <c1.aronrojas@gmail.com>
2025-05-13 19:36:37 -0300

    Función fetchone en psycopg2

---
+++ _3erSemestre/1.1.Python_3/Clase5/testDB/registros_fetchOne.py
@@ -0,0 +1,32 @@
+"""
+uso de fetchOne
+"""
+import psycopg2
+conexion = psycopg2.connect(user='postgres',password='admin',host='127.0.0.1',port='5432',database='test_db')
+
+try:
+    with conexion:
+        with conexion.cursor() as cursor:
+            sentencia = 'SELECT id_persona, nombre FROM persona'
+            cursor.execute(sentencia)   #  ejecutamos sentencia
+            registros = cursor.fetchall()   # Recuperamos todos los registros que serán una lista
+            print("Soy un registro: ",registros)
+            sentencia2 = 'SELECT * FROM persona WHERE id_persona = 1'
+            cursor.execute(sentencia2)   # ejecutamos sentencia
+            registros2 = cursor.fetchall()   # Recuperamos todos los registros que serán una lista
+            print("Soy un registro distinto: ",registros2)
+            sentencia3 = 'SELECT * FROM persona WHERE id_persona = %s'  #placeholder
+            id_persona = 1
+            cursor.execute(sentencia3,(id_persona,))   # ejecutamos sentencia y la variable establecida en el placeholder
+            registros3 = cursor.fetchall()   # Recuperamos todos los registros en una lista
+            print("Soy parametro establecido por un placeholder ",registros2)
+            sentencia4 = 'SELECT * FROM persona WHERE id_persona = %s'  #placeholder
+            id_persona = input('Digite un numero para el id_persona: ')
+            cursor.execute(sentencia4,(id_persona,))   # lo mismo pero con input
+            registros4 = cursor.fetchall()   # Recuperamos todos los registros en una lista
+            print("Soy parametro establecido por un placeholder ",registros2)
+except Exception as e:
+    print(f'Ocurrio un error: {e}')
+finally:
+    conexion.close()
+
--- _3erSemestre/1.1.Python_3/Clase5/testDB/registros_with.py
+++ _3erSemestre/1.1.Python_3/Clase5/testDB/registros_with.py
@@ -4,19 +4,18 @@ Uso de with y psycopg2
 www.psycopg.org/docs/usage.html
 """
 import psycopg2
-conexion = psycopg2.connect(
-    user='postgres',
-    password='admin',
-    host='127.0.0.1',
-    port='5432',
-    database='test_db'
-)
-with conexion:
-    with conexion.cursor() as cursor:
-        sentencia = 'SELECT * FROM persona'
-        cursor.execute(sentencia)   #  ejecutamos sentencia
-        registros = cursor.fetchall()   # Recuperamos todos los registros que serán una lista
-        print(registros)
 
-cursor.close()
-conexion.close()
+conexion = psycopg2.connect(user='postgres',password='admin',host='127.0.0.1',port='5432',database='test_db')
+
+try:
+    with conexion:
+        with conexion.cursor() as cursor:
+            sentencia = 'SELECT * FROM persona'
+            cursor.execute(sentencia)   #  ejecutamos sentencia
+            registros = cursor.fetchall()   # Recuperamos todos los registros que serán una lista
+            print(registros)
+except Exception as e:
+    print(f'Ocurrio un error: {e}')
+# cursor.close()    --> se cierra el llamado
+finally:
+    conexion.close()
