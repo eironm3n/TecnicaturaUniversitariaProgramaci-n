caafd4809dfc5497531da8d650e6ad5650d71618
Arón Rojas <c1.aronrojas@gmail.com>
2025-05-14 15:45:36 -0300

    Clase 3 de Java, forEach

---
+++ _3erSemestre/3. Java_3/Clase3/forEach/src/test/TestForEach.java
@@ -0,0 +1,14 @@
+/*
+Clase 3 - ForEach
+ */
+package test;
+
+public class TestForEach {
+    public static void main(String[] args) {
+        int edades[] = {5, 6, 8, 9};
+        for (int edad: edades) {
+            System.out.println("edad = " + edad);
+            
+        }
+    }
+}
