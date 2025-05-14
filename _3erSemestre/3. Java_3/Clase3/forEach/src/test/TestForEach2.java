f6c40efa3388edc221d4a86d642e52a8d0cdc6ed
Arón Rojas <c1.aronrojas@gmail.com>
2025-05-14 16:22:39 -0300

    Update corregido y funcional

--- _3erSemestre/3. Java_3/Clase3/forEach/src/test/TestForEach2.java
+++ _3erSemestre/3. Java_3/Clase3/forEach/src/test/TestForEach2.java
@@ -8,21 +8,22 @@ import domain.Persona;
 public class TestForEach2 {
     public static void main(String[] args) {
         int edades[] = {5, 6, 8, 9};
-            for(int edad: edades) {    //sintaxis del forEach
-                System.out.println("edad = " + edad);
-            }
-        
-            Persona personas[] = {new Persona("Juan"), new Persona("Carla"), new Persona("Beatriz")};
-            
-             //ForEach
-            for(var persona: personas){
-            System.out.println("persona = " + persona );
-            
-         /*
-         En clase se enseño esto, pero no funciona
-         for(Persona persona: personas){
-         System.out.println("persona = " + persona );
-         */
+        for(int edad: edades) {    //sintaxis del forEach
+            System.out.println("edad = " + edad);
         }
+        
+        Persona personas[] = {new Persona("Juan"), new Persona("Carla"), new Persona("Beatriz")};
+        System.out.println("Se ejecuta el bloque de personas");
+        
+        //ForEach
+        for(Persona persona: personas){
+            System.out.println("persona = " + persona );
+        }  
+        /*
+        Se uso la funcionalidad del Java:
+        Menú: Run --> Clean and Build Project
+        Luego: Run --> Run File sobre TestForEach2.java
+         */
+        
     }
 }
