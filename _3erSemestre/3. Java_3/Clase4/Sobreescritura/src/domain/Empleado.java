71764bd2adeca2034de31747a28867add14065db
Arón Rojas <c1.aronrojas@gmail.com>
2025-05-18 17:54:04 -0300

    finalizada la clase padre

--- _3erSemestre/3. Java_3/Clase4/Sobreescritura/src/domain/Empleado.java
+++ _3erSemestre/3. Java_3/Clase4/Sobreescritura/src/domain/Empleado.java
@@ -4,7 +4,38 @@ La sobreescritura ocurre cuando la clase hija crea una escritura sobre el compor
  */
 package domain;
 
+import java.security.ProtectionDomain;
+
 
 public class Empleado {
+    protected String nombre; 
+    protected double sueldo;
+    
+    public Empleado(String nombre,double sueldo){
+        this.nombre = nombre;
+        this.sueldo= sueldo;
+        }
+    
+    //Metodo para la sobreescritura
+    public String obtenerDetalles(){
+        return "Nombre: "+ this.nombre + ", Sueldo: "+this.sueldo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(double sueldo) {
+        this.sueldo = sueldo;
+    }
+    
     
 }
