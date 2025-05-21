763d2afff946a90c072f271734d6295caee82682
Arón Rojas <c1.aronrojas@gmail.com>
2025-05-21 19:55:09 -0300

    Se agregan las practicas

---
+++ _3erSemestre/3. Java_3/Clase7/Practica-Avatar-3raParte/js/avatar.js
@@ -0,0 +1,8 @@
+function seleccionarPersonajeJugador(){
+    alert('SELECCINASTE TU PERSONAJE')
+}   
+/* Agregar una condicion en donde mencione 'cual' fue el personaje elegido*/
+
+let botonPersonajeJugador = document.getElementById('boton-personaje');
+botonPersonajeJugador.addEventListener('click', seleccionarPersonajeJugador);
+
