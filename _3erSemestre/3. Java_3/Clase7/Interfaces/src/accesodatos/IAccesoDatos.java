fa8a8915d154f25247e5cebaba7aa2c0c77262a1
Arón Rojas <c1.aronrojas@gmail.com>
2025-05-22 14:57:10 -0300

    Update IAccesoDatos.java

--- _3erSemestre/3. Java_3/Clase7/Interfaces/src/accesodatos/IAccesoDatos.java
+++ _3erSemestre/3. Java_3/Clase7/Interfaces/src/accesodatos/IAccesoDatos.java
@@ -1,14 +1,22 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+Clase 7 - Interfaces en Java
+Al crear el archivo .java, se debe elegir el archivo de"Interfaces"
+Tambien al asignarle un nombre, debe colocarse la I al inicio
  */
 package accesodatos;
 
-/**
- *
- * @author Arón
- */
+// Pueden existir clases padres o hijas que sean 'interface'
 public interface IAccesoDatos {
+    int MAX_REGISTRO = 10;
     
+    
+    // Metodo insertar es abstracto y sin cuerpo
+    void insertar();
+    
+    void listar();
+    
+    void actualizar();
+    
+    void eliminar();
+    // esto tambien se utiliza asi para insertar en base de datos
 }
