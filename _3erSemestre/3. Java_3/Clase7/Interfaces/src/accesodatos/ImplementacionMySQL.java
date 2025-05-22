675d873377cbb93d860a9b1ce5abee3301240a3d
Arón Rojas <c1.aronrojas@gmail.com>
2025-05-22 16:38:06 -0300

    parte 2

---
+++ _3erSemestre/3. Java_3/Clase7/Interfaces/src/accesodatos/ImplementacionMySQL.java
@@ -0,0 +1,29 @@
+/*
+Implementacion de comportamiento de Interface-sql
+ */
+package accesodatos;
+
+public class ImplementacionMySQL implements IAccesoDatos{
+    // elegimos la opcion 'Implement all abstract methods'
+    
+    @Override
+    public void insertar() {
+        
+    }
+
+    @Override
+    public void listar() {
+
+    }
+
+    @Override
+    public void actualizar() {
+
+    }
+
+    @Override
+    public void eliminar() {
+
+    }
+    
+}
