31517097aa026b5c5cdce89170ab342ebca5794b
Arón Rojas <c1.aronrojas@gmail.com>
2025-05-22 16:53:38 -0300

    Ultima parte y final de la clase 7.

--- _3erSemestre/3. Java_3/Clase7/Interfaces/src/test/TestInterfaces.java
+++ _3erSemestre/3. Java_3/Clase7/Interfaces/src/test/TestInterfaces.java
@@ -8,6 +8,15 @@ import accesodatos.*;
 public class TestInterfaces {
     public static void main(String[] args) {
         IAccesoDatos datos = new ImplementacionMySQL();
+        //datos.listar();
+        imprimir(datos);
+        
+        datos = new ImplementacionOracle();
+        //datos.listar();
+        imprimir(datos);
+    }
+    
+    public static void imprimir(IAccesoDatos datos) {
         datos.listar();
     }
 }
