d7670059d07b6d64cdc27ea6e6677c590020c864
Arón Rojas <c1.aronrojas@gmail.com>
2025-05-22 16:42:50 -0300

    parte 3

--- _3erSemestre/3. Java_3/Clase7/Interfaces/src/accesodatos/ImplementacionMySQL.java
+++ _3erSemestre/3. Java_3/Clase7/Interfaces/src/accesodatos/ImplementacionMySQL.java
@@ -8,22 +8,22 @@ public class ImplementacionMySQL implements IAccesoDatos{
     
     @Override
     public void insertar() {
-        
+        System.out.println("Insertar desde MySQL");
     }
 
     @Override
     public void listar() {
-
+        System.out.println("Listar desde MySQL");
     }
 
     @Override
     public void actualizar() {
-
+        System.out.println("Actualizar desde MySQL");
     }
 
     @Override
     public void eliminar() {
-
+        System.out.println("Eliminar desde MySQL");
     }
     
 }
