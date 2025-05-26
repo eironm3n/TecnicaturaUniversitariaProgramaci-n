6a503da9f7c5abe3011038a52c7cb41f5ff0513d
Arón Rojas <c1.aronrojas@gmail.com>
2025-05-26 20:33:30 -0300

    Update conexion.py

--- _3erSemestre/1.1.Python_3/Clase8/capa_datos_persona/conexion.py
+++ _3erSemestre/1.1.Python_3/Clase8/capa_datos_persona/conexion.py
@@ -1,6 +1,9 @@
 """
 Comenzamos con atributos de clase
 """
+import psycopg2 as bd
+from logger_base import *
+import sys
 
 class Conexion:
     _DATABASE = 'test_db'
@@ -11,6 +14,21 @@ class Conexion:
     _conexion = None
     _cursor = None
 
-    
 
+    @classmethod
+    def obtenerConexion(cls):
+        if cls._conexion is None:
+            try:
+                cls._conexion = bd.connect(host=cls._HOST,
+                                           user=cls._USERNAME,
+                                           password=cls._PASSWORD,
+                                           port=cls._DB_PORT,
+                                           database=cls._DATABASE)
 
+                log.debug(f'Conexión Exitosa: {cls._conexion}')
+                return cls._conexion
+            except Exception as e:
+                log.error(f'Ocurrio un error: {e}')
+                sys.exit()
+            else:
+                return cls._conexion
