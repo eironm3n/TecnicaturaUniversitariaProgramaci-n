d55c585065f36f058cecc053d857760c64a9215c
Arón Rojas <c1.aronrojas@gmail.com>
2025-05-26 20:05:59 -0300

    Update

--- _3erSemestre/1.1.Python_3/Clase8/capa_datos_persona/Persona.py
+++ _3erSemestre/1.1.Python_3/Clase8/capa_datos_persona/Persona.py
@@ -3,6 +3,8 @@ Clase 8 Capa de Datos: Logging y Postgresql Parte 2
 
 """
 
+from logger_base import log
+
 class Persona:
     def __init__(self, id_persona,nombre,apellido,email):
         self._id_persona = id_persona
@@ -11,5 +13,56 @@ class Persona:
         self._email = email
 
 
+    def __str__(self):
+        return f'''
+            ID Persona: {self._id_persona},
+            Nombre: {self._nombre},
+            Apellido: {self._apellido},
+            Email: {self._email}
+        '''
 
-        
+    # Metodos Getters and Setters
+
+    # Para id_persona
+    @property
+    def id_persona(self):
+        return self._id_persona
+    
+    @id_persona.setter
+    def id_persona(self, id_persona):
+        self._id_persona = id_persona
+
+    # Para nombre
+    @property
+    def nombre(self):
+        return self._nombre
+    
+    @nombre.setter
+    def nombre(self, nombre):
+        self._nombre = nombre
+
+    # Para apellido
+    @property
+    def apellido(self):
+        return self._apellido
+    
+    @apellido.setter
+    def apellido(self, apellido):
+        self._apellido = apellido
+
+    # Para email
+    @property
+    def email(self):
+        return self._email
+    
+    @email.setter
+    def email(self, email):
+        self._email = email
+
+
+if __name__=='__main__':
+    persona1 = Persona(1,'Juan','Perez','jperez@mail.com')
+    log.debug(persona1)
+    persona2 = Persona('Jose','Leppez','jleppez@mail.com')
+    log.debug(persona2)
+
---
+++ _3erSemestre/1.1.Python_3/Clase8/capa_datos_persona/logger_base.py
@@ -0,0 +1,24 @@
+
+import logging as log
+
+log.basicConfig(level=log.DEBUG,
+                format='%(asctime)s:%(levelname)s [%(filename)s:%(lineno)s] %(message)s',
+                datefmt='%I:%M:%S %p',  #datefmt es el formato de la fecha
+                handlers=[log.StreamHandler(),])
+
+# Llamamos una configuracion basica
+if __name__ == '__main__':
+    log.debug('Mensaje a nivel debug')
+    log.info('Mensaje a nivel info')
+    log.warning('Mensaje a nivel Warning')
+    log.error('Mensaje a nivel error')
+    log.critical('Mensaje a nivel critical')
+
+"""
+La terminal arrojaria =
+DEBUG:root:Mensaje a nivel debug
+INFO:root:Mensaje a nivel info
+WARNING:root:Mensaje a nivel Warning
+ERROR:root:Mensaje a nivel error
+CRITICAL:root:Mensaje a nivel critical
+"""
