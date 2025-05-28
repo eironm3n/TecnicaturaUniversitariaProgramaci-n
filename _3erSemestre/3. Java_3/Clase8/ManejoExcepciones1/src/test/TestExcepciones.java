ba43f49d3574908a12a309e7b03e8ab295053cd5
Arón Rojas <c1.aronrojas@gmail.com>
2025-05-28 19:21:25 -0300

    Update Manejo de excepciones

---
+++ _3erSemestre/3. Java_3/Clase8/ManejoExcepciones1/src/test/TestExcepciones.java
@@ -0,0 +1,15 @@
+/*
+Manejo de excepciones
+ */
+package test;
+
+
+public class TestExcepciones {
+    public static void main(String[] args) {
+        int resultado = 10/0;
+        System.out.println("resultado = " + resultado);
+        
+        //Esto dará una salida de error, lo siguiente, será para que el programa continue y no pare
+        
+    }
+}
