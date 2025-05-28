c681be222c92b1e25b5484a39b46b235ac43585f
Arón Rojas <c1.aronrojas@gmail.com>
2025-05-28 19:11:50 -0300

    Comienzo con clase 8 de Java, javabeans y excepciones.

---
+++ _3erSemestre/3. Java_3/Clase8/JavaBeans/src/domain/Persona.java
@@ -0,0 +1,44 @@
+/*
+Clase 8 - JavaBeans 
+ */
+package domain;
+
+public class Persona implements Serializable{
+    private String nombre;
+    private String apellido;
+    
+    // Constructor vacio: esto es obligatorio para el Javabeans
+    public Persona(){
+        
+    }
+    
+    public Persona(String nombre, String apellido){
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    //Se agregan Getters and Setters y se agrega encapsulamiento
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+    
+    //Se agregan toString
+
+    @Override
+    public String toString() {
+        return "Persona{" + "nombre=" + nombre + ", apellido=" + apellido + '}';
+    }
+    
+}
