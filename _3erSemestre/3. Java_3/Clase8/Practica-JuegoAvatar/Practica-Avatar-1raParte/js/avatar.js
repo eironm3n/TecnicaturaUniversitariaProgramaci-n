fce1dd93e376f2975ba8ebd3c58557f6375e0cc3
Arón Rojas <c1.aronrojas@gmail.com>
2025-05-28 20:44:58 -0300

    Update - continuamos

--- _3erSemestre/3. Java_3/Clase8/Practica-JuegoAvatar/Practica-Avatar-1raParte/js/avatar.js
+++ _3erSemestre/3. Java_3/Clase8/Practica-JuegoAvatar/Practica-Avatar-1raParte/js/avatar.js
@@ -4,10 +4,22 @@ function iniciarJuego(){
 }
 
 function seleccionarPersonajeJugador(){
-    if(document.getElementById('zuko').checked){
-        alert('Seleccionaste a Zuko')
+    let inputZuko = document.getElementById('zuko')
+    let inputKatara = document.getElementById('katara')
+    let inputAang = document.getElementById('aang')
+    let inputToph = document.getElementById('toph')
+
+    if(inputZuko.checked){
+    alert('Seleccionaste a Zuko')
+    }else if(inputKatara.checked){
+    alert('Seleccionaste a Katara')
+    }else if(inputAang.checked){
+    alert('Seleccionaste a Aang')
+    }else if(inputToph.checked){
+    alert('Seleccionaste a Toph')
     }
 }
+
 
 
 
