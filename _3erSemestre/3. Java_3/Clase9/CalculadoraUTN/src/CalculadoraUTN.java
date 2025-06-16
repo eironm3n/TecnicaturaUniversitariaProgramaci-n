a93f7df7a4323e9275c92b59d7c8b20522774131
Arón Rojas <c1.aronrojas@gmail.com>
2025-06-16 18:37:59 -0300

    Create CalculadoraUTN.java

---
+++ _3erSemestre/3. Java_3/Clase9/CalculadoraUTN/src/CalculadoraUTN.java
@@ -0,0 +1,14 @@
+/*
+* Comienzo Proyecto UTN - Calculadora con IntelliJ
+*
+* */
+
+public class CalculadoraUTN {
+    public static void main(String[] args) {
+        System.out.println("*********** Aplicacion Calculadora ***********");
+        var operando1 = 10;
+        var operando2 = 20;
+        var resultado = operando1 + operando2;
+        System.out.println("Resultado = " + resultado);
+    }
+}
