62820dbbf49129ddfee3f43a18a70c47ed006448
Arón Rojas <c1.aronrojas@gmail.com>
2025-06-16 19:01:13 -0300

    Update CalculadoraUTN.java

--- _3erSemestre/3. Java_3/Clase9/CalculadoraUTN/src/CalculadoraUTN.java
+++ _3erSemestre/3. Java_3/Clase9/CalculadoraUTN/src/CalculadoraUTN.java
@@ -9,11 +9,23 @@ public class CalculadoraUTN {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         System.out.println("*********** Aplicacion Calculadora ***********");
-        System.out.println("Digite el valor para el operando1: ");
-        var operando1 = Integer.parseInt(entrada.nextLine());
-        System.out.println("Digite el valor para el operando2: ");
-        var operando2 = Integer.parseInt(entrada.nextLine());;
-        var resultado = operando1 + operando2;
-        System.out.println("resultado = " + resultado);
+        //Mostramos el menú
+        System.out.println("""
+                1. Suma
+                2. Resta
+                3. Multiplicación
+                4. Division
+                5. Salir
+                """);
+        System.out.print("Operacion a realizar ?");
+        var operacion = Integer.parseInt(entrada.nextLine());
+
+        if(operacion >= 1 && operacion <= 4){
+            System.out.println("Digite el valor para el operando1: ");
+            var operando1 = Integer.parseInt(entrada.nextLine());
+            System.out.println("Digite el valor para el operando2: ");
+            var operando2 = Integer.parseInt(entrada.nextLine());;
+        }
+
     }
 }
