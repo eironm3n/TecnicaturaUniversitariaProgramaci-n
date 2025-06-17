a7ed39b7f3256d0e1b9351f944f08550ab16f3eb
Arón Rojas <c1.aronrojas@gmail.com>
2025-06-16 21:48:57 -0300

    cambios

--- _3erSemestre/3. Java_3/Clase9/Practica-JuegoAvatar/Practica-Avatar-1raParte/js/avatar.js
+++ _3erSemestre/3. Java_3/Clase9/Practica-JuegoAvatar/Practica-Avatar-1raParte/js/avatar.js
@@ -4,22 +4,43 @@ function iniciarJuego(){
 }
 
 function seleccionarPersonajeJugador(){
-    let personajeAleatorio = aleatorio(1,4);
+    let inputZuko = document.getElementById('zuko')
+    let inputKatara = document.getElementById('katara')
+    let inputAang = document.getElementById('aang')
+    let inputToph = document.getElementById('toph')
     let spanPersonajeJugador = document.getElementById('personaje-jugador')
 
-    if(personajeAleatorio == 1){
-        spanPersonajeJugador.innerHTML = 'Piñaso'
-    }else if(personajeAleatorio == 2){
-        spanPersonajeJugador.innerHTML = 'Patadaso'
-    }else if(personajeAleatorio == 3){
-        spanPersonajeJugador.innerHTML = 'Barridaso'
+    if(inputZuko.checked){
+        spanPersonajeJugador.innerHTML = 'Zuko'
+    }else if(inputKatara.checked){
+        spanPersonajeJugador.innerHTML = 'Katara'
+    }else if(inputAang.checked){
+        spanPersonajeJugador.innerHTML = 'Aang'
+    }else if(inputToph.checked){
+        spanPersonajeJugador.innerHTML = 'Toph'
     }else{
         alert('Selecciona un personaje')
     }
+    seleccionarPersonajeEnemigo()
 }
 
 function aleatorio(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function seleccionarPersonajeEnemigo(){
+    let personajeAleatorio = aleatorio(1,4)
+    let spanPersonajeEnemigo = document.getElementById('personaje-enemigo')
+
+    if(personajeAleatorio == 1){
+        spanPersonajeEnemigo.innerHTML = 'Zuko'
+    }else if(personajeAleatorio == 2){
+        spanPersonajeEnemigo.innerHTML = 'Katara'
+    }else if(personajeAleatorio == 3){
+        spanPersonajeEnemigo.innerHTML = 'Aang'
+    }else{
+        spanPersonajeEnemigo.innerHTML = 'Toph'
+    }
 }
 
 windows.addEventListener('load',iniciarJuego);
