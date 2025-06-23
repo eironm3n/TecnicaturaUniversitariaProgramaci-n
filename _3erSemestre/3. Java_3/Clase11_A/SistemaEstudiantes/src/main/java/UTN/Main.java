fa5feba63c0ea54af31aa5adeeffa8a6666ad1ae
Arón Rojas <c1.aronrojas@gmail.com>
2025-06-23 17:24:39 -0300

    Instalación MavenConector y configuración del mismo

--- _3erSemestre/3. Java_3/Clase11_A/SistemaEstudiantes/src/main/java/UTN/Main.java
+++ _3erSemestre/3. Java_3/Clase11_A/SistemaEstudiantes/src/main/java/UTN/Main.java
@@ -1,6 +1,12 @@
 /*
 * Se comienza con el proyecto Sistema Estudiantes de la Clase 11 de Java 3
-* */
+* Se realiza la descarga desde google de: maven main-connector java
+* https://mvnrepository.com/artifact/mysql/mysql-connector-java
+* Esto se inserta dentro del archivo xml dentro de la etiqueta dependencias
+* No olvidar sincronizar desde la pestaña Maven, en 'sincronize'
+ */
+
+
 package UTN;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -11,6 +17,5 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.print("Hello and welcome!");
 
-        }
     }
 }
