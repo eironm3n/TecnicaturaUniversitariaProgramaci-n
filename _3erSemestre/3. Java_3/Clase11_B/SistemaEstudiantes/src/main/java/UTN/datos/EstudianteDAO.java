a98ce026e485612502746cc27913f2444ecfec7c
Arón Rojas <c1.aronrojas@gmail.com>
2025-06-23 18:49:56 -0300

    Finalizado, comenzamos con las pruebas del método listarEstudiantes() -> solucionamos algunos errores de conexión

--- _3erSemestre/3. Java_3/Clase11_B/SistemaEstudiantes/src/main/java/UTN/datos/EstudianteDAO.java
+++ _3erSemestre/3. Java_3/Clase11_B/SistemaEstudiantes/src/main/java/UTN/datos/EstudianteDAO.java
@@ -2,6 +2,8 @@ package UTN.datos;
 
 import UTN.dominio.Estudiante;
 
+import static UTN.conexion.Conexion.getConnection;
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
@@ -16,6 +18,36 @@ public class EstudianteDAO {
         PreparedStatement ps;   // Este nos ayuda envia la sentencia a la base de datos
         ResultSet rs;   // Obtenemos el resultado de la base de datos
 
-        
-    }
+        // Creamos un objeto de tipo conexión
+        Connection con = getConnection();
+        String sql = "SELECT * FROM estudiantes ORDER BY estudiantes2025";
+        try{
+            ps = con.prepareStatement(sql);
+            rs = ps.executeQuery();
+            while (rs.next()){
+                var estudiante = new Estudiante();
+                estudiante.setIdEstudiante(rs.getInt("idestudiantes2025"));
+                estudiante.setNombre(rs.getString("nombre"));
+                estudiante.setApellido(rs.getString("apellido"));
+                estudiante.setTelefono(rs.getString("telefono"));
+                estudiante.setEmail(rs.getString("email"));
+
+                //Falta agregarlo a la lista
+                estudiantes.add(estudiante);
+            }
+        }
+        catch (Exception e){
+            System.out.println("Ocurrio un error al seleccionar datos: "+e.getMessage());
+        }
+        finally {
+            try {
+                con.close();
+            }
+            catch (Exception e){
+                System.out.println("Ocurrio un error al cerra la conexion: "+e.getMessage());
+            }
+        }   //Fin finally
+
+        return estudiantes;
+    }   //Fin metodo Listar
 }
