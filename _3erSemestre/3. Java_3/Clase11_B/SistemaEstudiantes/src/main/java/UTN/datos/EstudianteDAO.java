e5ed40c04ef4c40ddf2c34417742202c1fa207b0
Arón Rojas <c1.aronrojas@gmail.com>
2025-06-24 18:35:44 -0300

    Metodo agregar un nuevo estudiante

--- _3erSemestre/3. Java_3/Clase11_B/SistemaEstudiantes/src/main/java/UTN/datos/EstudianteDAO.java
+++ _3erSemestre/3. Java_3/Clase11_B/SistemaEstudiantes/src/main/java/UTN/datos/EstudianteDAO.java
@@ -83,6 +83,34 @@ public class EstudianteDAO {
         return false;
     }
 
+    //Metodo agregar un nuevo estudiante
+    public boolean agregarEstudiante(Estudiante estudiante){
+        PreparedStatement ps;
+        Connection con = getConnection();
+        String sql = "INSERT INTO estudiantes2025 (nombre,apellido,telefono,email) VALUES (?,?,?,?)";
+        try{
+            ps = con.prepareStatement(sql);
+            ps.setString(1,estudiante.getNombre());
+            ps.setString(2,estudiante.getApellido());
+            ps.setString(3,estudiante.getTelefono());
+            ps.setString(4,estudiante.getEmail());
+            ps.execute();
+            return true;
+        }
+        catch (Exception e){
+            System.out.println("Ocurrio un error al agregar estudiante: "+e.getMessage());
+        } //Fin catch
+        finally {
+            try{
+                con.close();
+            }
+            catch (Exception e){
+                System.out.println("Error al cerrar la conexion: "+e.getMessage());
+            } //Fin catch
+        } //Fin finally
+        return false;
+    } // Fin metodo agregarEstudiante
+
     public static void main(String[] args){
         //Listar los estudiantes
         var estudianteDao = new EstudianteDAO();
