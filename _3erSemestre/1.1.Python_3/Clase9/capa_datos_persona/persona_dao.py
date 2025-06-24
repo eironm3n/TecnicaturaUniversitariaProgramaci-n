3a4b719af5871947feccbad3c837e9b328d25bab
Arón Rojas <c1.aronrojas@gmail.com>
2025-06-24 20:20:21 -0300

    Finalizado el CRUD, se corrige el error del metodoSeleccionar

--- _3erSemestre/1.1.Python_3/Clase9/capa_datos_persona/persona_dao.py
+++ _3erSemestre/1.1.Python_3/Clase9/capa_datos_persona/persona_dao.py
@@ -45,12 +45,44 @@ class PersonaDAO:
                 log.debug(f'Persona Insertada: {persona}')
                 return cursor.rowcount
             
+    @classmethod
+    def actualizar(cls,persona):
+        with Conexion.obtenerConexion():
+            with Conexion.obtenerCursor() as cursor:
+                valores = (persona.nombre, persona.apellido, persona.email, persona.id_persona)
+                cursor.execute(cls._ACTUALIZAR,valores)
+                log.debug(f'Persona Actualizada: {persona}')
+                return cursor.rowcount
+            
+    @classmethod
+    def eliminar(cls,persona):
+        with Conexion.obtenerConexion():
+            with Conexion.obtenerCursor() as cursor:
+                valores = (persona.id_persona,)
+                cursor.execute(cls._ELIMINAR,valores)
+                log.debug(f'Los objetos eliminados son: {persona}')
+                return cursor.rowcount
 
 if __name__=='__main__':
+
+    #Eliminar un registro
+    persona1 = Persona(id_persona=1)
+    personas_eliminadas = PersonaDAO.eliminar(persona1)
+    log.debug(f'Resgistro de persona eliminada: {personas_eliminadas}')
+
+    """
+    #Actualizar un registro
+    persona1 = Persona(1,nombre='Homero',apellido='Ramos',email='promero@mail.com')
+    personas_actualizadas = PersonaDAO.actualizar(persona1)
+    log.debug(f'Personas actualizadas: {personas_actualizadas}')
+    """
+    
+    """
     # Insertar registro
-    persona1 = Persona(nombre='Pedro',apellido='Romero',email='promero@mail.com')
+    persona1 = Persona(nombre='Homero',apellido='Ramos',email='promero@mail.com')
     personas_insertadas = PersonaDAO.insertar(persona1)
     log.debug(f'Personas insertadas: {personas_insertadas}')
+    """
 
     # Seleccionar objetos
     personas = PersonaDAO.seleccionar()
