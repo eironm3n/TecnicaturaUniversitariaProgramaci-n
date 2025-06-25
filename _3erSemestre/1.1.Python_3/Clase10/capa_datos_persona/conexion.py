4cfc8041aa0da60d96eeaca12e7141d985443a91
Arón Rojas <c1.aronrojas@gmail.com>
2025-06-25 19:53:10 -0300

    Update conexion.py

--- _3erSemestre/1.1.Python_3/Clase10/capa_datos_persona/conexion.py
+++ _3erSemestre/1.1.Python_3/Clase10/capa_datos_persona/conexion.py
@@ -22,44 +22,14 @@ class Conexion:
 
     @classmethod
     def obtenerConexion(cls):
-        pass
-    """
-        if cls._conexion is None:
-            try:
-                cls._conexion = bd.connect(host=cls._HOST,
-                                           user=cls._USERNAME,
-                                           password=cls._PASSWORD,
-                                           port=cls._DB_PORT,
-                                           database=cls._DATABASE)
+        conexion = cls.obtenerPool().getconn()
+        
 
-                log.debug(f'Conexión Exitosa: {cls._conexion}')
-                return cls._conexion
-            except Exception as e:
-                log.error(f'Ocurrio un error: {e}')
-                sys.exit()
-        else:
-            return cls._conexion
-    """
     
     @classmethod
     def obtenerCursor(cls):
         pass
-    """
-        if cls._cursor is None:
-            try:
-                cls._cursor = cls.obtenerConexion().cursor()
-                log.debug(f'Se abrio correctamente el cursor: {cls._cursor}')
-                return cls._cursor
-            except Exception as e:
-                log.error(f'Ocurrio un error: {e}')
-                sys.exit
-        else:
-            return cls._cursor
 
-if __name__ == '__main__':
-    Conexion.obtenerConexion()
-    Conexion.obtenerCursor()
-    """
 
     @classmethod
     def obtenerPool(cls):
