bf454364c94b01bd4a8d3d8951dd4b3f50e67f49
Arón Rojas <c1.aronrojas@gmail.com>
2025-06-25 20:25:00 -0300

    Update cursor_del_pool.py

--- _3erSemestre/1.1.Python_3/Clase11/capa_datos_persona/cursor_del_pool.py
+++ _3erSemestre/1.1.Python_3/Clase11/capa_datos_persona/cursor_del_pool.py
@@ -11,3 +11,16 @@ class CursorDelPool:
         self._conexion = Conexion.obtenerConexion()
         self._cursor = self._conexion.cursor()
         return self._cursor
+    
+    def __exit__(self,tipo_exception,valor_exception,detalle_exception):
+        log.debug('Se ejecuta el metodo exit')
+        if valor_exception:
+            self._conexion.rollback()
+            log.debug(f'Ocurrio una excepcion: {valor_exception}')
+        else:
+            self._conexion.commit()
+            log.debug('Commit de la transacción')
+        self._cursor.close()
+        Conexion.liberarConexion(self._conexion)
+
+
