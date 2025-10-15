77aec6c8d9261d9f2337321d444d70d2d6134ad2
Arón Rojas <c1.aronrojas@gmail.com>
2025-10-15 14:50:01 -0300

    Finalizada clase 07

---
+++ _4toSemestre/1.1.Python/Clase 07/Juego_starwars/constantes.py
@@ -0,0 +1,11 @@
+import os
+
+# Dimensiones de la pantalla, el alto y ancho de la pantalla
+SCREEN_WIDTH = 1700
+SCREEN_HEIGHT = 1000
+
+# Especificamos los colores de la pantalla
+COLOR_LASER = (0, 0, 255)  # Azul
+
+# Ruta a la carpeta assets
+ASSETS_PATH = os.path.join(os.path.dirname(__file__), 'assets_1')
---
+++ _4toSemestre/1.1.Python/Clase 07/Juego_starwars/main.py
@@ -0,0 +1,8 @@
+import pygame
+import sys
+import random
+import os
+from personajes import Personaje , Enemigo , Explosion
+from constantes import SCREEN_WIDTH , SCREEN_HEIGHT , ASSETS_PATH
+
+
